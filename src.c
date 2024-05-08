33d2c89cc3e0ef9166435a155f3eeb4ca6bbc1aa
Mariam-Mourad <88984744+Mariam-Mourad@users.noreply.github.com>
2024-05-08 13:51:55 +0300

    inserting error in src.c

--- src.c
+++ src.c
@@ -5,7 +5,7 @@ int main() {
     int t1 = 0, t2 = 1; // Initialize the first two terms
     int nextTerm = t1 + t2;
 
-    printf("Enter the number of terms: ");
+    printf("Enter the number of terms: ")
     scanf("%d", &n);
 
     // Print the first two terms
@@ -19,5 +19,5 @@ int main() {
         nextTerm = t1 + t2;
     }
 
-    return 0;
+    return 0
 }
